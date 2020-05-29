653ab0b78005bd4f2f032aa64975892465e0ab21
Alex <pich.alex01@gmail.com>
2020-05-29 04:23:09 -0700

    Dynamic sstyled components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-// import "./Person.css";
-
 const StyledDiv = styled.div`
   width: 60%;
   margin: 16px auto;
@@ -17,14 +15,7 @@ const StyledDiv = styled.div`
 `;
 
 const person = (props) => {
-  //   const style = {
-  //     "@media(min-width: 500px)": {
-  //       width: "450px",
-  //     },
-  //   };
-
   return (
-    // <div className="Person" style={style}>
     <StyledDiv>
       <p onClick={props.click}>
         I'm {props.name} and {props.age} years old.
@@ -32,7 +23,6 @@ const person = (props) => {
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
     </StyledDiv>
-    // </div>
   );
 };
 
