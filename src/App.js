3c090cbb81f9b7fe195f4c18ba1c00223b6e81d9
Alex <pich.alex01@gmail.com>
2020-05-29 04:34:58 -0700

    pre eject

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 
 import "./App.css";
 import Person from "./Person/Person";
 
-const StyledButton = styled.button`
-  background-color: ${(props) => (props.myAlt ? "red" : "green")};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-  &:hover {
-    background-color: ${(props) => (props.myAlt ? "salmon" : "lightgreen")};
-    color: black;
-  }
-`;
+// const StyledButton = styled.button`
+//   background-color: ${(props) => (props.myAlt ? "red" : "green")};
+//   color: white;
+//   font: inherit;
+//   border: 1px solid blue;
+//   padding: 8px;
+//   cursor: pointer;
+//   &:hover {
+//     background-color: ${(props) => (props.myAlt ? "salmon" : "lightgreen")};
+//     color: black;
+//   }
+// `;
 
 class App extends Component {
   state = {
@@ -111,12 +110,12 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(" ")}>This is working</p>
-        <StyledButton
-          myAlt={this.state.showPersons}
+        <button
+          className="button"
           onClick={() => this.togglePersonsHandler("Alex!!")}
         >
           Toggle Persons
-        </StyledButton>
+        </button>
         {persons}
       </div>
     );
