8ba0499d1b5ffaba8f342ef5d0fa8c42585882fd
Alex <pich.alex01@gmail.com>
2020-05-28 17:03:14 -0700

    Adding annd using Radium

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Radium from "radium";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -53,6 +54,10 @@ class App extends Component {
       border: "1px solid blue",
       padding: "8px",
       cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black",
+      },
     };
 
     let persons = null;
@@ -74,6 +79,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black",
+      };
     }
 
     const classes = [];
@@ -100,4 +109,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
